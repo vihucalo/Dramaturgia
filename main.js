37c42045286bebb5a1f4d2cc0261646ee9c4fd84
vihucalo <57508103+vihucalo@users.noreply.github.com>
2021-06-26 12:16:51 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+var user_id, opcion;
+opcion = 4;
+    
+tablaUsuarios = $('#tablaUsuarios').DataTable({  
+    "ajax":{            
+        "url": "bd/crud.php", 
+        "method": 'POST', //usamos el metodo POST
+        "data":{opcion:opcion}, //enviamos opcion 4 para que haga un SELECT
+        "dataSrc":""
+    },
+    "columns":[
+        {"data": "id"},
+        {"data": "nombre"},
+        {"data": "autor"},
+        {"data": "pais"},
+        {"data": "femenino"},
+        {"data": "masculino"},
+        {"data": "otros"},
+        {"defaultContent": "<div class='text-center'><div class='btn-group'><button class='btn btn-primary btn-sm btnEditar'><i class='material-icons'>edit</i></button><button class='btn btn-danger btn-sm btnBorrar'><i class='material-icons'>delete</i></button></div></div>"}
+    ]
+});     
+         
+
+//para limpiar los campos antes de dar de Alta una Persona
+$("#btnNuevo").click(function(){
+    opcion = 1; //alta           
+    id=null;
+    $("#formUsuarios").trigger("reset");
+    $(".modal-header").css( "background-color", "#17a2b8");
+    $(".modal-header").css( "color", "white" );
+    $(".modal-title").text("Buscar");
+    $('#modalCRUD').modal('show');	    
+});
+
+//Editar        
+$(document).on("click", ".btnEditar", function(){		        
+    opcion = 2;//editar
+    fila = $(this).closest("tr");	        
+    id = parseInt(fila.find('td:eq(0)').text()); //capturo el ID		            
+    nombre = fila.find('td:eq(1)').text();
+    autor = fila.find('td:eq(2)').text();
+    pais = fila.find('td:eq(3)').text();
+    femenino = fila.find('td:eq(4)').text();
+    masculino = fila.find('td:eq(5)').text();
+    otros = fila.find('td:eq(6)').text();
+    $("#username").val(username);
+    $("#nombre").val(nombre);
+    $("#autor").val(last_name);
+    $("#pais").val(gender);
+    $("#femenino").val(password);
+    $("#masculino").val(status);
+    $(".modal-header").css("background-color", "#007bff");
+    $(".modal-header").css("color", "white" );
+    $(".modal-title").text("Editar Usuario");		
+    $('#modalCRUD').modal('show');		   
+});
+
+//Borrar
+$(document).on("click", ".btnBorrar", function(){
+    fila = $(this);           
+    id = parseInt($(this).closest('tr').find('td:eq(0)').text()) ;		
+    opcion = 3; //eliminar        
+    var respuesta = confirm("¿Está seguro de borrar el registro "+id+"?");                
+    if (respuesta) {            
+        $.ajax({
+          url: "bd/crud.php",
+          type: "POST",
+          datatype:"json",    
+          data:  {opcion:opcion, id:id},    
+          success: function() {
+              tablaUsuarios.row(fila.parents('tr')).remove().draw();                  
+           }
+        });	
+    }
+ });
+     
+});    
